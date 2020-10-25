e2a7dd19d6cc67239dcd05e093724ca8a5e25898
Raphael Vidal <raphael.r.vidal@gmail.com>
2020-10-25 10:51:31 -0300

    cofiguração de perfil no db para user

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Eloquent implements AuthenticatableContract
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','perfil'
     ];
 
     /**
